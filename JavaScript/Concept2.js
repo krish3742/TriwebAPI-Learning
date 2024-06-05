f5737a7ed3c8434c2d28bd49feb57942d852542b
krish3742 <krish3742@gmail.com>
2024-06-05 16:54:57 +0530

    Practiced the Prototype, Iterator and Generator concepts

--- JavaScript/Concept2.js
+++ JavaScript/Concept2.js
@@ -95,3 +95,49 @@ newSet.add(15);
 newSet.add(15);
 newSet.add(16);
 console.log(newSet);
+
+// let obj = {
+//     fullName: 'Kshitij',
+//     mobile: 6389769480,
+//     display() {
+//         console.log(`${this.fullName}`);
+//     },
+// };
+
+// // let obj1 = Object.create(obj);
+// let obj1 = {address: "India"};
+// obj1.__proto__ = obj;
+// console.log(obj1);
+// obj1.fullName = 'Kshitij Agrawal';
+// console.log(obj1);
+// console.log(obj1.__proto__);
+// obj1.display();
+
+// console.log();
+
+// let getSet = {
+//     age: 18,
+//     get getAge() {
+//         console.log(this.age);
+//     },
+//     set setAge(newAge) {
+//         this.age = newAge;
+//     }
+// };
+
+// let kshitij = Object.create(getSet);
+// kshitij.setAge = 22;
+// kshitij.getAge;
+// console.log(kshitij);
+
+// function* makeIterator() {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+// }
+// const iter = makeIterator();
+
+// console.log(iter);
+// for (const itItem of iter) {
+//     console.log(itItem);
+// }
