221b23c6088387e7a522b2a421004a3a9cd5ea5a
krish3742 <krish3742@gmail.com>
2024-06-06 14:13:24 +0530

    Practiced template literals

--- ES6/Concept1.js
+++ ES6/Concept1.js
@@ -1,8 +1,12 @@
 let a;
 let name = "Kshitij";
 const b = 5;
-
 console.log(a);
 console.log(b);
 console.log(name);
 let type = typeof b;
+
+let str = `Hello! ${1 + 2 + 3 + 4 + 5}`;
+console.log(str);
+console.log(`Hello! My rating is ${1 + 2 + 3 + 4}`);
+
