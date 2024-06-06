64bd06df7d64bbf2b1c697d54ca55bdc45f44233
krish3742 <krish3742@gmail.com>
2024-06-06 14:15:59 +0530

    Practied Arroe functions

--- ES6/Concept1.js
+++ ES6/Concept1.js
@@ -10,3 +10,16 @@ let str = `Hello! ${1 + 2 + 3 + 4 + 5}`;
 console.log(str);
 console.log(`Hello! My rating is ${1 + 2 + 3 + 4}`);
 
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+  }
+console.log(addTwoNumbers(4, 5));
+let multiplyNumbers = (number1, number2) => {
+return number1*number2;
+}
+console.log(multiplyNumbers(5, 6));
+console.log(addTwoNumbers);
+console.log(multiplyNumbers);
+multiplyNumbers = 7;
+console.log(multiplyNumbers(5, 6));
+
