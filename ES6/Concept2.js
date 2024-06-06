4d2bf6b8f57787d76a0adb7c71f1d061dd8e188f
krish3742 <krish3742@gmail.com>
2024-06-06 14:21:55 +0530

    Studied about map and set objects

--- ES6/Concept2.js
+++ ES6/Concept2.js
@@ -8,3 +8,24 @@ if('fullName' in obj) {   // true
 if('name' in obj) {    // false
     console.log(obj.mobile);
 }
+
+
+let map = new Map();
+console.log(map);
+map.set('Name', 'Kshitij Agrawal');
+map.set('Mobile', 6389769480);
+console.log(map);
+console.log(map.get('Name'));
+
+let set = new Set([1, 2, 5, 5, 7, 7, 9]);
+console.log(set);
+set.add(15);
+set.add('Foo');
+console.log(set);
+
+let newSet = new Set();
+newSet.add(13);
+newSet.add(15);
+newSet.add(15);
+newSet.add(16);
+console.log(newSet);
