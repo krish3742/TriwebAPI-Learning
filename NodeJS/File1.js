e54996f80c200a57a1a201fc760a358c96d6bcf8
krish3742 <krish3742@gmail.com>
2024-06-15 15:37:54 +0530

    Created the simple calculator using REST API

---
+++ NodeJS/File1.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello");
+});
+
+app.post("/add", (req, res) => {
+    res.send(`Result = ${req.body.num1 + req.body.num2}`);
+});
+
+app.post("/subtract", (req, res) => {
+    res.send(`Result = ${req.body.num1 - req.body.num2}`);
+});
+
+app.post("/multiply", (req, res) => {
+    res.send(`Result = ${req.body.num1 * req.body.num2}`);
+});
+
+app.post("/division", (req, res) => {
+    res.send(`Result = ${req.body.num1 / req.body.num2}`);
+});
+
+app.post("/remainder", (req, res) => {
+    res.send(`Result = ${req.body.num1 % req.body.num2}`);
+});
+
+app.listen(3000);
