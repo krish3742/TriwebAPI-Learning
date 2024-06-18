a4eb1ae68351531e8c855433e52d4d6776b7dd5f
krish3742 <krish3742@gmail.com>
2024-06-18 22:29:38 +0530

    Practiced even no and odd no program

--- NodeJS/Programmin_OOPs/break_continue.js
+++ NodeJS/Programmin_OOPs/break_continue.js
@@ -5,9 +5,19 @@ for (let y = 0; y < 5; y++) {
     console.log(y);
 }
 
+console.log();
+
 for (let y = 0; y < 5; y++) {
     if(y == 3) {
         continue;
     }
     console.log(y);
+}
+
+for (let i = 1; i <=20; i++) {
+    if(i % 2 == 0){
+        console.log(`Number is even ${i}`);
+    } else {
+        console.log(`Number is odd ${i}`);
+    }
 }
