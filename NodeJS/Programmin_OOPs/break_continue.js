555561b5c7860953fa9f54be25efe66bcf2e47b2
krish3742 <krish3742@gmail.com>
2024-06-18 22:25:54 +0530

    Practiced break and continue statements

---
+++ NodeJS/Programmin_OOPs/break_continue.js
@@ -0,0 +1,13 @@
+for (let y = 0; y < 5; y++) {
+    if(y == 3) {
+        break;
+    }
+    console.log(y);
+}
+
+for (let y = 0; y < 5; y++) {
+    if(y == 3) {
+        continue;
+    }
+    console.log(y);
+}
