0e50f8b70de1d1335cd5b0f05f5c8a559ac55558
krish3742 <krish3742@gmail.com>
2024-07-02 19:27:28 +0530

    Removed the unwanted code from our files

--- Projects/MERN Todo App/Backend NodeJS/router/list.js
+++ Projects/MERN Todo App/Backend NodeJS/router/list.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const listController = require('../controller/list');
 router.post("/add", listController.insert);
 router.get("/get", listController.fetch);
-router.put("/update", listController.update)
+router.put("/update", listController.update);
 router.delete("/delete", listController.remove);
 
 module.exports = router;
