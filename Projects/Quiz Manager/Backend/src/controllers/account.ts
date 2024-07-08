9716b171013092320a959331192d24caaa52478f
krish3742 <krish3742@gmail.com>
2024-07-08 18:18:56 +0530

    Created controllers for activate and deactivate account

--- Projects/Quiz Manager/Backend/src/controllers/account.ts
+++ Projects/Quiz Manager/Backend/src/controllers/account.ts
@@ -1,16 +1,66 @@
 import { RequestHandler } from "express";
 import { returnResponse } from "../utils/interfaces";
-const activateAccount: RequestHandler = (req, res, next) => {
+import User from "../models/user";
+import ProjectError from "../helpers/projectError";
+
+const activateAccount: RequestHandler = async (req, res, next) => {
     let resp: returnResponse;
     try {
-        res.send("Activated");
+        const givenId = req.params.userId;
+        const userId = req.userId;
+        if(givenId !== userId) {
+            const err = new ProjectError("You are not authorized");
+            err.statusCode = 401;
+            throw err;
+        }
+        const user = await User.findById(userId);
+        if(!user) {
+            const err = new ProjectError("User not found!");
+            err.statusCode = 404;
+            throw err;
+        }
+        user.activate = true;
+        user.deactivate = false;
+        await user.save();
+        resp = {
+            status: "success",
+            message: "Account reactivated!",
+            data: {}
+        }
+        res.send(resp);
     } catch (error) {
         next(error);
     }
 };
 
-const deactivateAccount: RequestHandler = (req, res, next) => {
-    res.send("Reactivated!");
+const deactivateAccount: RequestHandler = async (req, res, next) => {
+    let resp: returnResponse;
+    try {
+        const givenId = req.params.userId;
+        const userId = req.userId;
+        if(userId !== givenId) {
+            const err = new ProjectError("You are not authorized!");
+            err.statusCode = 401;
+            throw err;
+        }
+        const user = await User.findById(userId);
+        if(!user) {
+            const err = new ProjectError("User not found!");
+            err.statusCode = 404;
+            throw err;
+        }
+        user.activate = false;
+        user.deactivate = true;
+        await user.save();
+        resp = {
+            status: "success",
+            message: "Account deactivated!",
+            data: {}
+        }
+        res.send(resp);
+    } catch (error) {
+        next(error);
+    }
 };
 
 export {activateAccount, deactivateAccount};
