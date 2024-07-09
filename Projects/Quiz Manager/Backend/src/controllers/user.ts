6f16d830b163c4895e8679eceeb54a12137a15df
krish3742 <krish3742@gmail.com>
2024-07-09 16:02:56 +0530

    Created changePassword route

--- Projects/Quiz Manager/Backend/src/controllers/user.ts
+++ Projects/Quiz Manager/Backend/src/controllers/user.ts
@@ -2,6 +2,8 @@ import { RequestHandler } from "express";
 import User from '../models/user';
 import ProjectError from '../helpers/projectError';
 import { returnResponse } from "../utils/interfaces";
+import bcrypt from 'bcryptjs';
+import { validationResult } from "express-validator";
 
 const fetchUser: RequestHandler = async (req, res, next) => {
     let resp: returnResponse;
@@ -15,7 +17,7 @@ const fetchUser: RequestHandler = async (req, res, next) => {
         const result = await User.findById(userId, {name: 1, email: 1});
         if(!result) {
             const err = new ProjectError("User not found!");
-            err.statusCode = 401;
+            err.statusCode = 404;
             throw err;
         } else {
             resp = {
@@ -65,4 +67,55 @@ const updateUser: RequestHandler = async (req, res, next) => {
     }
 };
 
-export { fetchUser, updateUser };
+const changePassword: RequestHandler = async (req, res, next) => {
+    let resp: returnResponse;
+    try {
+        const userId = req.body._id;
+        const validationError = validationResult(req);
+        if(!validationError.isEmpty()) {
+            const err = new ProjectError("Validation Error!");
+            err.statusCode = 422;
+            err.data = validationError.array();
+            throw err;
+        }
+        if(userId != req.userId) {
+            const err = new ProjectError("You are not authorized!");
+            err.statusCode = 403;
+            throw err;
+        }
+        const user = await User.findById(userId);
+        if(!user) {
+            const err = new ProjectError("User not found!");
+            err.statusCode = 401;
+            throw err;
+        }
+        const currentPassword = req.body.currentPassword;
+        const newPassword = req.body.newPassword;
+        const newEncryptedPassword = await bcrypt.hash(newPassword, 12);
+        const checkingPassword = await bcrypt.compare(currentPassword, user.password);
+        if(!checkingPassword) {
+            const err = new ProjectError("Current password is incorrect! Please try again...");
+            err.statusCode = 401;
+            throw err;
+        }
+        const checkingNewPassword = await bcrypt.compare(currentPassword, newEncryptedPassword);
+        if(checkingNewPassword) {
+            const err = new ProjectError("Current password is same as new password!");
+            err.statusCode = 401;
+            throw err;
+        }
+        user.password = newEncryptedPassword;
+        await user.save();
+        resp = {
+            status: "success",
+            message: "Password changed!",
+            data: {}
+        };
+        res.send(resp);
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+export { fetchUser, updateUser, changePassword };
--- Projects/Quiz Manager/Backend/src/routes/user.ts
+++ Projects/Quiz Manager/Backend/src/routes/user.ts
@@ -1,12 +1,24 @@
 import express  from "express";
 const router = express.Router();
-import { fetchUser, updateUser } from "../controllers/user";
+import { fetchUser, updateUser, changePassword } from "../controllers/user";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
 import { isDeactivated} from '../middlewares/isDeactivated';
+import { body } from "express-validator";
 
 //GET /user/:userID
 router.get('/:userId', isAuthenticated, isDeactivated, fetchUser);
 //PUT /user
 router.put("/", isAuthenticated, isDeactivated, updateUser);
+//PUT /user/changepassword
+router.put('/changepassword', isAuthenticated, isDeactivated, [
+    body('confirmPassword')
+        .trim()
+        .custom((confirmPassword, {req}) => {
+            if(req.body.newPassword !== confirmPassword) {
+                return Promise.reject("Confirm password mismatch!")
+            }
+            return true;
+        })
+], changePassword);
 
 export default router;
